57fe340b27d32ded1d1b05b3a8a0bd7851072b79
prithivi-10 <112410257+prithivi-10@users.noreply.github.com>
2022-10-04 13:40:33 +0530

    Add files via upload

---
+++ sunday.java
@@ -0,0 +1,13 @@
+package sunday;
+
+public class sunday {
+
+    public static void main(String srgs[]){
+
+        String str=";";
+
+        System.out.println(str.matches("."));
+
+    }
+    
+}
